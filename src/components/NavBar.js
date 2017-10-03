796f5dd91cc17ef8bc76219bf9915ef4cc411f86
Erin Grady <eringrady@Erins-MacBook-Air.local>
2017-10-03 15:52:37 -0400

    added color to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-
+import '../styles/index.css'
 
 export default class NavBar extends Component {
   render () {
     return (
       <div class="container">
-        <nav class="navbar navbar-dark bg-primary">
-          Play What!?
+        <nav class="navbar">
+          <img src="../public/favicon.ico" alt=""/>
+          <h1>Play What!?</h1>
         </nav>
       </div>
-          )   
+          )
 
   }
 }
